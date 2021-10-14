343ebd6d61cb9c645f787b754a33194d5ed7555f
M-O-H <mohmed114.salah@gmail.com>
2021-10-14 19:17:44 +0200

    commit message

--- auth/passport.js
+++ auth/passport.js
@@ -15,6 +15,7 @@ passport.use(new GoogleStrategy({
 	clientID: process.env.GOOGLE_CLIENT_ID ,
 	clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 	callbackURL: "http://localhost:3001/auth/google/redirect",
+	proxy: true 
 },
 	function(accessToken, refreshToken, profile, done) {
 		// check user on MongoDB 
