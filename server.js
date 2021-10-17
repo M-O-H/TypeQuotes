2ca28fb0cd0932d807708928e67ee61935a353dc
M-O-H <mohmed114.salah@gmail.com>
2021-10-17 22:43:01 +0200

    commit message

--- server.js
+++ server.js
@@ -50,16 +50,7 @@ if(process.env.NODE_ENV === 'production'){
 		  url = url.substring(1);
 		res.sendFile(url);
 	  });
-
-	  const reqType = req.headers["x-forwarded-proto"];
-	  // if not https redirect to https unless logging in using OAuth
-	  if (reqType !== "https") {
-		  req.url.indexOf("auth/google") !== -1
-			? next()
-			: res.redirect("https://" + req.headers.host + req.url);
-	  } 
-  } else {
-	  next();
+	  
 }
 
 app.listen(PORT, () => {
