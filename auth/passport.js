1bab4f118b18c1d53e8e5da1fce2ad78fc6d94ee
M-O-H <mohmed114.salah@gmail.com>
2021-12-01 19:22:29 +0200

    refactor redirect

--- auth/passport.js
+++ auth/passport.js
@@ -17,7 +17,8 @@ passport.use(new GoogleStrategy({
 	clientID: process.env.GOOGLE_CLIENT_ID ,
 	clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 	callbackURL: herokuCallBack,
-	proxy: true 
+	proxy: true ,
+	passReqToCallback   : true
 },
 	function(accessToken, refreshToken, profile, done) {
 		// check user on MongoDB 
