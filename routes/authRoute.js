8627121e844eea2d89728df86ff06aae52b5d6a2
M-O-H <mohmed114.salah@gmail.com>
2021-12-04 21:17:09 +0200

    redesign profile component

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,7 +3,7 @@ const express = require('express')
 const passportConfig = require('../auth/passport');
 const router = express.Router();
 const successUrl = "/profile"
-const TsuccessUrl = "https://tping-game.herokuapp.com/profile"
+const TsuccessUrl = "https://tping-game.herokuapp.com/"
 const failedUrl = "https://tping-game.herokuapp.com/"
 
 router.route('/google').get(passport.authenticate('google', { scope:[ 'email', 'profile' ] }))
