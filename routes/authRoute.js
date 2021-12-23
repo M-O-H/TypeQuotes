c5ca9c9c579c89c219b19894701b710c603c06f2
Mohamed Salah <47217195+M-O-H@users.noreply.github.com>
2021-12-23 15:51:22 +0200

    Update authRoute.js

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,15 +3,14 @@ const express = require('express')
 const passportConfig = require('../auth/passport');
 const router = express.Router();
 const successUrl = "/profile"
-const TsuccessUrl = "https://tping-game.herokuapp.com/"
-const failedUrl = "https://tping-game.herokuapp.com/"
+const failedUrl = "/"
 
 router.route('/google').get(passport.authenticate('google', { scope:[ 'email', 'profile' ] }))
 
 router.route('/google/callback').get(
 	passport.authenticate('google', { 
 	  failureRedirect: failedUrl,
-	  successRedirect: TsuccessUrl,
+	  successRedirect: successUrl,
 	  failureMessage: "Cannot logging in to Google, please try again later!",
 	})
 );
